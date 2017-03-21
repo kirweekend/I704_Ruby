e87b3f30e5d4aa8d865c91824492b15697322189
Kirill <kirweekend@mail.ru>
2017-03-21 23:48:41 +0200

    add state game over

--- project/game.rb
+++ project/game.rb
@@ -16,6 +16,7 @@ module Game
       super(WIDTH, HEIGHT)
       self.caption = "Space Game"
       @background_image = Gosu::Image.new("assets/sky.jpg", :tileable => true)
+      @image = Gosu::Image.new("assets/heart.png")
       @player = Player.new(self)
       @player.size(PLAYER_WIDTH, PLAYER_HEIGHT)
       @bullets = []
@@ -23,7 +24,8 @@ module Game
       @explosions = []
       @score = 0
       @beep = Gosu::Sample.new "assets/shoot.ogg"
-      @font = Gosu::Font.new(20)
+      @boom = Gosu::Sample.new "assets/explosion.ogg"
+      @font = Gosu::Font.new(40)
     end
 
     def button_down(id)
@@ -34,12 +36,22 @@ module Game
     end
 
     def draw
-      @player.draw()
-      @bullets.each(&:draw)
-      @bombs.each(&:draw)
-      @explosions.each(&:draw)
-      @background_image.draw(0,0,0)
-      @font.draw("Score: #{score}", 10, 10, 3, 1.0, 1.0, Gosu::Color::YELLOW)
+      if @new_game
+        @new_game.draw("Final Score: #{@score}", 315, 320, 4, 1.0, 1.0, Gosu::Color::YELLOW)
+        @new_game.draw("Press Return to Try Again", 200, 270, 4, 1.0, 1.0, Gosu::Color::YELLOW)
+        @background_image.draw(0,0,0)
+        @bullets = []
+        @bombs = []
+        @explosions = []
+      else
+        @player.draw()
+        @bullets.each(&:draw)
+        @bombs.each(&:draw)
+        @explosions.each(&:draw)
+        @background_image.draw(0,0,0)
+        @image.draw(40, 15, 3)
+        @font.draw("#{score}", 100, 14, 3, 1.0, 1.0, Gosu::Color::YELLOW)
+      end
     end
 
     def update
@@ -59,8 +71,17 @@ module Game
             @explosions << Explosion.new(self, bomb.x, bomb.y)
             @bombs.delete bomb
             @bullets.delete bullet
+            @boom.play
             @score += 10
           end
+        end
+      end
+      @bombs.each do |bomb|
+        distance_p = Gosu.distance(bomb.x, bomb.y, @player.x, @player.y)
+        if distance_p < bomb.radius + @player.radius
+          @explosions << Explosion.new(self, bomb.x, bomb.y)
+          @bombs.delete bomb
+          @new_game = Gosu::Font.new(50)
         end
       end
       @explosions.each do |explosion|
@@ -71,6 +92,16 @@ module Game
       end
       @bullets.each do |bullet|
         @bullets.delete bullet unless bullet.onscreen?
+      end
+
+      if @new_game and button_down? Gosu::KbReturn
+        @new_game = nil
+        @score = 0
+        @player = Player.new(self)
+        @player.size(PLAYER_WIDTH, PLAYER_HEIGHT)
+        @bullets = []
+        @bombs = []
+        @explosions = []
       end
     end
   end
